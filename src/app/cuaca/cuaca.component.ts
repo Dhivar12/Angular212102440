936caa6fc77c43303d23b6c6466a64d5a87d0b13
Dhivar12 <147850464+Dhivar12@users.noreply.github.com>
2023-12-15 11:26:38 +0700

    angular

---
+++ src/app/cuaca/cuaca.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cuaca',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cuaca.component.html',
+  styleUrl: './cuaca.component.css'
+})
+export class CuacaComponent {
+
+}
